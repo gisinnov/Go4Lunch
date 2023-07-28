ec653d86310074a8920cc2be2017f248f5196898
Gis <126614881+gisinnov@users.noreply.github.com>
2023-07-28 10:33:12 +0200

    update ic

---
+++ app/src/main/java/com/example/go4lunch/ui/map/MapFragement.java
@@ -0,0 +1,38 @@
+package com.example.go4lunch.ui.map;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProvider;
+
+import com.example.go4lunch.databinding.FragmentMapBinding;
+import com.example.go4lunch.ui.map.MapViewModel;
+
+public class MapFragment extends Fragment {
+
+    private FragmentMapBinding binding;
+
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             ViewGroup container, Bundle savedInstanceState) {
+        MapViewModel mapViewModel =
+                new ViewModelProvider(this).get(MapViewModel.class);
+
+        binding = FragmentMapBinding.inflate(inflater, container, false);
+        View root = binding.getRoot();
+
+        final TextView textView = binding.textMap;
+        MapViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
+        return root;
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        binding = null;
+    }
+}
