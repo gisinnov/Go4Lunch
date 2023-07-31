7b76452425171251e6a57228a57325c3898d1bbe
Robot <robot@macbook-pro-de-robot.home>
2023-07-31 09:19:22 +0200

    update caméra

--- app/src/main/java/com/example/go4lunch/SignUpActivity.java
+++ app/src/main/java/com/example/go4lunch/SignUpActivity.java
@@ -1,23 +1,32 @@
 package com.example.go4lunch;
 
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
 import android.os.Bundle;
 import android.widget.Toast;
-
-import android.content.pm.PackageManager;
-
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.lifecycle.ViewModelProvider;
 
 import com.example.go4lunch.databinding.ActivitySignUpBinding;
+import com.example.go4lunch.Permissions;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
+import com.squareup.picasso.Picasso;
 
 public class SignUpActivity extends AppCompatActivity {
     private ActivitySignUpBinding binding;
     private AuthViewModel viewModel;
     private FirebaseFirestore firestore;
+
+    private static final int PICK_IMAGE_REQUEST = 1;
+    private StorageReference storageRef;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,6 +36,7 @@ public class SignUpActivity extends AppCompatActivity {
 
         firestore = FirebaseFirestore.getInstance();
         viewModel = new ViewModelProvider(this).get(AuthViewModel.class);
+        storageRef = FirebaseStorage.getInstance().getReference();
 
         binding.btnSignUp.setOnClickListener(v -> {
             String firstName = binding.etFirstName.getText().toString().trim();
@@ -38,7 +48,7 @@ public class SignUpActivity extends AppCompatActivity {
 
         binding.btnPickImage.setOnClickListener(v -> {
             if (Permissions.hasCameraPermission(this)) {
-                // Access the camera.
+                openFileChooser();
             } else {
                 Permissions.requestCameraPermission(this);
             }
@@ -52,14 +62,52 @@ public class SignUpActivity extends AppCompatActivity {
         });
     }
 
+    private void openFileChooser() {
+        Intent intent = new Intent();
+        intent.setType("image/*");
+        intent.setAction(Intent.ACTION_GET_CONTENT);
+        startActivityForResult(intent, PICK_IMAGE_REQUEST);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == PICK_IMAGE_REQUEST && resultCode == RESULT_OK && data != null && data.getData() != null) {
+            Uri imageUri = data.getData();
+
+            StorageReference imageRef = storageRef.child("images/" + imageUri.getLastPathSegment());
+
+            UploadTask uploadTask = imageRef.putFile(imageUri);
+            uploadTask.addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                @Override
+                public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                    Toast.makeText(SignUpActivity.this, "Image uploaded", Toast.LENGTH_SHORT).show();
+
+                    // Set the profileImage ImageView with the uploaded image
+                    imageRef.getDownloadUrl().addOnSuccessListener(new OnSuccessListener<Uri>() {
+                        @Override
+                        public void onSuccess(Uri uri) {
+                            Picasso.get().load(uri).into(binding.profileImage);
+                        }
+                    });
+                }
+            }).addOnFailureListener(new OnFailureListener() {
+                @Override
+                public void onFailure(@NonNull Exception exception) {
+                    Toast.makeText(SignUpActivity.this, "Failed to upload image", Toast.LENGTH_SHORT).show();
+                }
+            });
+        }
+    }
+
     @Override
     public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults);
         if (requestCode == Permissions.CAMERA_PERMISSION_REQUEST_CODE) {
             if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                // The camera permission was granted. You can access the camera here.
+                openFileChooser();
             } else {
-                // The camera permission was denied.
                 Toast.makeText(this, "Camera permission is required", Toast.LENGTH_SHORT).show();
             }
         }
