3381d3a3d6744fc52b0a12549b275163e1a66b67
Robot <robot@macbook-pro-de-robot.home>
2023-07-31 19:45:11 +0200

    update map api

--- app/src/main/java/com/example/go4lunch/ui/map/MapFragment.java
+++ app/src/main/java/com/example/go4lunch/ui/map/MapFragment.java
@@ -12,10 +12,15 @@ import androidx.lifecycle.ViewModelProvider;
 
 import com.example.go4lunch.databinding.FragmentMapBinding;
 import com.example.go4lunch.ui.map.MapViewModel;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
 
-
-public class MapFragment extends Fragment {
+public class MapFragment extends Fragment implements OnMapReadyCallback {
     private FragmentMapBinding binding;
+    private GoogleMap mMap;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
@@ -25,15 +30,53 @@ public class MapFragment extends Fragment {
         binding = FragmentMapBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
 
+        // Initialization of the Map
+        binding.mapView.onCreate(savedInstanceState);
+        binding.mapView.getMapAsync(this);
+
         final TextView textView = binding.textMap;
         mapViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
         return root;
     }
 
     @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+
+        // Add a marker in Cannes and move the camera
+        LatLng cannes = new LatLng(43.5523, 7.0174);
+        mMap.addMarker(new MarkerOptions().position(cannes).title("Marker in Cannes"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(cannes));
+    }
+
+    @Override
     public void onDestroyView() {
         super.onDestroyView();
+        binding.mapView.onDestroy();
         binding = null;
     }
-}
 
+    @Override
+    public void onResume() {
+        super.onResume();
+        binding.mapView.onResume();
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        binding.mapView.onPause();
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        binding.mapView.onStart();
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        binding.mapView.onStop();
+    }
+}
